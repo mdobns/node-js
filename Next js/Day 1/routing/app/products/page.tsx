a2d26992e577160641e9f510fe1f6a4dd3e0fe33
Md Oshama Bin Nur <mdobns@hotmail.com>
2025-09-18 15:47:28 +0600

    dynamic routing is done

---
+++ Next js/Day 1/routing/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function Products(){
+    return(
+        <>
+        <h1>Products</h1>
+        <ul>Product 1</ul>
+        <ul>Product 2</ul>
+        <ul>Product 3</ul>
+        <ul>Product 4</ul>
+        </>
+    )
+}
