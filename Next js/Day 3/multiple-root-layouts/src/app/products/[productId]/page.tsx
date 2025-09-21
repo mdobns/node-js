186fca2b7072e6c7f7d4645829b5175e2969604e
Md Oshama Bin Nur <mdobns@hotmail.com>
2025-09-22 00:22:39 +0600

    use client uses with metadata

--- Next js/Day 3/multiple-root-layouts/src/app/products/[productId]/page.tsx
+++ Next js/Day 3/multiple-root-layouts/src/app/products/[productId]/page.tsx
@@ -1,6 +1,23 @@
-export default async function Product( {params }:{params : Promise<{ productId: string}>})
-    {
-        const productId = (await params).productId
-    
-    return <h1>The details of product {productId}</h1>
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ( {params}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const name = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Iphone ${id}`);
+        }, 100);
+    });
+    return {
+        title: `Product ${name}`,
+    };
+}
+
+export default async function Product({ params }: Props) {
+  const productId = (await params).productId;
+
+  return <h1>The details of product {productId}</h1>;
 }
